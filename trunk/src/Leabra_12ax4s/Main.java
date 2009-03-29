fe886f1ed5b8a47d2634c99d6743e6399e66124f
David.Jilk <David.Jilk@400ec7d0-0e44-11de-b321-f573fdaff03e>
2009-03-29 03:09:31 +0000

    Updated main for actual run test

--- trunk/src/Leabra_12ax4s/Main.java
+++ trunk/src/Leabra_12ax4s/Main.java
@@ -17,19 +17,35 @@ public class Main {
         EmergentConnection E = new EmergentConnection("127.0.0.1");
         try {
             E.Initialize();
+            //E.SetDebug(true);
             EmergentDataTable TrainEnv_0 = new EmergentDataTable("TrainEnv_0", new TrainEnv_0Row(), E);
             EmergentDataTable UnitNames = new EmergentDataTable("UnitNames", new UnitNamesRow(), E);
             EmergentDataTable TrialOutputData = new EmergentDataTable("TrialOutputData", new TrialOutputDataRow(), E);
             EmergentDataTable EpochOutputData = new EmergentDataTable("EpochOutputData", new EpochOutputDataRow(), E);
-            //TrainEnv_0.Read();
-            //UnitNames.Read();  // Errors reading both of these guys 
-            E.RunProgram("CPT12axEpoch");
+            /*
+            TrainEnv_0.Read();
+            for (EmergentDataTableRow R : TrainEnv_0) {
+                TrainEnv_0Row Row = (TrainEnv_0Row) R;
+                System.out.println(Row.Group + " " + Row.Name + " " + Row.Input[0][0] + " " + Row.RewTarg[0][0]); }
+             
+            UnitNames.Read();  // Errors reading both of these guys 
+            for (EmergentDataTableRow R : UnitNames) {
+                UnitNamesRow Row = (UnitNamesRow) R;
+                System.out.println(Row.Input[0][0] + " " + Row.Output[0][0] + " " + Row.RewTarg[0][0]); }
+             */
+            E.RunProgram("LeabraEpochGpData");
             TrialOutputData.Read();  // Doesn't handle empty table - throws exception
+            for (EmergentDataTableRow R : TrialOutputData) {
+                TrialOutputDataRow Row = (TrialOutputDataRow) R;
+                System.out.println(Row.trial_name + " " + Row.sse);
+            }
+            /*
             EpochOutputData.Read();
             for (EmergentDataTableRow R : TrialOutputData) {
                 TrialOutputDataRow TR = (TrialOutputDataRow) R;
                 System.out.println(TR.batch + " " + TR.epoch + " " + TR.trial + " " + TR.sse);
             }
+             */
             E.Shutdown();
         }
         catch (EmergentException e) {
